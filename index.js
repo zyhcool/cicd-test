5037c4c5cf2e4bc9dfe86ee5e4638993016d8cdc
zyhcool <13105027620@163.com>
2020-10-23 16:46:43 +0800

    0.0.2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ router.get('/', async (ctx, next) => {
     ctx.body = {
         message: `hit ${port}`,
         publishTime,
-        version: '0.0.1'
+        version: '0.0.2'
 
     };
     ctx.status = 200;
